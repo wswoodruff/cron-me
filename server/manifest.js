fdc26031f70ca3e926cf7777101523aae077870c
wswoodruff <williamswoodruff@gmail.com>
2019-06-23 00:52:12 -0400

    Add missing AWS creds to .env-keep

--- server/manifest.js
+++ server/manifest.js
@@ -41,7 +41,7 @@ module.exports = new Confidence.Store({
                         region: process.env.AWS_REGION,
                         accessKeyId: process.env.AWS_ACCESS_KEY_ID,
                         secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-                        SQSUrl: process.env.AWS_SQS_QUEUE,
+                        SQSUrl: process.env.AWS_SQS_QUEUE_URL,
                         SQSArn: process.env.AWS_SQS_QUEUE_ARN,
                         SQSId: process.env.AWS_SQS_QUEUE_ID
                     }
